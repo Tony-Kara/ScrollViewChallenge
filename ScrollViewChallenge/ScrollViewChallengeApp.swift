6589a528deb210f1bfce3e612877682409e12bd9
Tony-Kara <eniolaanthony@gmail.com>
2021-09-22 21:14:14 +0600

    Initial Commit

---
+++ ScrollViewChallenge/ScrollViewChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollViewChallengeApp.swift
+//  ScrollViewChallenge
+//
+//  Created by mac on 9/22/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrollViewChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
